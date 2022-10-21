ca44e7612d038ffc792088eea995d42b3c850fe1
Krout0n <me+git@tokinia.me>
2022-10-21 15:55:40 +0900

    Add AST, Object, eval

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,25 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum AST {
+    Num(usize),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Object {
+    Num(usize),
+}
+
+pub fn eval(ast: AST) -> Object {
+    match ast {
+        AST::Num(v) => Object::Num(v),
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn test_eval() {
+        let ast = AST::Num(1);
+        assert_eq!(eval(ast), Object::Num(1));
     }
 }
