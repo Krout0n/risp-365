7d4873af92b1f8f6a5d8c2282a1d66c1a73fd631
Krout0n <me+git@tokinia.me>
2022-10-21 17:21:14 +0900

    Add `Add`

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 #[derive(Debug, Clone, PartialEq)]
 pub enum AST {
     Num(usize),
+    Add(Box<AST>, Box<AST>),
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum Object {
-    Num(usize),
-}
-
-pub fn eval(ast: AST) -> Object {
+pub fn eval(ast: AST) -> usize {
     match ast {
-        AST::Num(v) => Object::Num(v),
+        AST::Num(v) => v,
+        AST::Add(left, right) => {
+            let left_obj = eval(*left);
+            let right_obj = eval(*right);
+            left_obj + right_obj
+        }
     }
 }
 
@@ -20,6 +21,24 @@ mod tests {
     #[test]
     fn test_eval() {
         let ast = AST::Num(1);
-        assert_eq!(eval(ast), Object::Num(1));
+        assert_eq!(eval(ast), 1);
+
+        // (1+2)
+        let simple_add = AST::Add(Box::new(AST::Num(1)), Box::new(AST::Num(2)));
+        assert_eq!(eval(simple_add), 3);
+
+        // ((((1+2)+3)+4)+5)
+        let complicated_add = AST::Add(
+            Box::new(AST::Add(
+                Box::new(AST::Add(
+                    Box::new(AST::Add(Box::new(AST::Num(1)), Box::new(AST::Num(2)))),
+                    Box::new(AST::Num(3)),
+                )),
+                Box::new(AST::Num(4)),
+            )),
+            Box::new(AST::Num(5)),
+        );
+
+        assert_eq!(eval(complicated_add), 15);
     }
 }
